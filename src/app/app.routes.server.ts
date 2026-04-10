a9ddba83d722ee4f9363c2cad7679b40a4448f9a
Jhoan <reyesjhoan52119@gmail.com>
2026-04-09 23:09:59 -0500

    Cambio en el app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productos/:id',
+    renderMode: RenderMode.Client // <-- Cambia 'mode' por 'renderMode'
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // <-- Aquí también
   }
 ];
